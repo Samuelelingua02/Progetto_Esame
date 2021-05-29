ae97156565e74050d213a8c4f2b59cf650a13846
Samuele Lingua <Samuelelingua17@gmail.com>
2021-05-29 09:29:02 +0200

    heroku try

--- server.js
+++ server.js
@@ -1,52 +1,21 @@
-"use strict"
-const fs = require('fs');
-const ERRORS = require('errors');
-const cookieParser = require('cookie-parser')
-const HTTPS = require('https');
-
-// code 600 - database connection error
-ERRORS.create({
-    code: 600,
-    name: 'DB_CONNECTION',
-    defaultMessage: 'An error occured when connecting to database'
-});
-
-// code 601 - query execution error
-ERRORS.create({
-    code: 601,
-    name: 'QUERY_EXECUTE',
-    defaultMessage: 'An error occured during the query execution'
-});
-
-// mongo
-const MONGO_CLIENT = require("mongodb").MongoClient;
-const CONNECTION_STRING = 'mongodb://127.0.0.1:27017';
-const CONNECTION_OPTIONS = {
-    useNewUrlParser: true,
-    //useUnifiedTopology: true
-};
 
 // express
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
-const { create } = require('errors');
 
 //EJS//////
 //app.set('view engine', 'ejs');
 
 // Google Auth
-//const {OAuth2Client} = require('google-auth-library');
-//const CLIENT_ID = '759412275936-j36b15qkpndslm139qcj50dsvv449vso.apps.googleusercontent.com'
-//const client = new OAuth2Client(CLIENT_ID);
+const {OAuth2Client} = require('google-auth-library');
+const CLIENT_ID = '759412275936-j36b15qkpndslm139qcj50dsvv449vso.apps.googleusercontent.com'
+const client = new OAuth2Client(CLIENT_ID);
 
 
 // avvio server
-const TIMEOUT = 300; // 60 SEC
 let pageNotFound;
-const PORT = 1337 || process.env.PORT;
+const PORT = process.env.PORT || 1337;
 
 
 app.listen(PORT, function() {
